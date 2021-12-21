c718a4f4cef7114dc9e641410628a348f13d6840
AntonioGomes42 <br.com.antoniogomes@gmail.com>
2021-12-21 12:29:35 -0300

    fixing env variables problem

--- repository/getDBData.js
+++ repository/getDBData.js
@@ -2,17 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue, goOffline, goOnline } from "firebase/database";
 import { readFile } from 'fs/promises';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 // Require and Consts
+const envVariables = dotenv.config()
 const firebaseConfig = JSON.parse(await readFile(new URL('./firebaseConfig.json', import.meta.url)));
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
-// dotenv.config(await readFile(new URL('../.env', import.meta.url)));
 
 async function getDBData(){
-    goOnline(db);
-    return new Promise((res) => {
-                onValue(ref(db, `ADPBSCADM1/program`), (snapshot) => {
+  goOnline(db);
+  return new Promise((res) => {
+                onValue(ref(db, `${process.env.DATA_PATH}`), (snapshot) => {
                 const returnedDbData = snapshot.val();
                 const transformedDbData = JSON.parse(returnedDbData);
                 goOffline(db);
